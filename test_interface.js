ff70aee9aa55687b9733264427d94e38b79cf7de
Aurélien <git@garnier.dev>
2022-12-12 18:07:13 +0100

    Fix cypress tests

--- test_interface.js
+++ test_interface.js
@@ -1,7 +1,7 @@
 const houses = [
-    ['206', '2', '0', 'Ouest', 170511],
-    ['187', '1', '1', 'Est', 170491],
-    ['194', '3', '1', 'Est', 170531],
+    ['206', '2', '0', 'Ouest', 254450],
+    ['187', '1', '1', 'Est', 230594],
+    ['194', '3', '1', 'Est', 271613],
 ];
 
 describe('Case 1', () => {
@@ -10,9 +10,9 @@ describe('Case 1', () => {
 
         const [size, nb_rooms, garden, _, price] = houses[0];
 
-        cy.get('input[aria-label="Taille maison"]').type(size);
-        cy.get('input[aria-label="Nombre de chambre"]').type(nb_rooms);
-        cy.get('input[aria-label="Y a un jardin"]').type(garden);
+        cy.get('input[aria-label="Taille maison"]').clear().type(size);
+        cy.get('input[aria-label="Nombre de chambre"]').clear().type(nb_rooms);
+        cy.get('input[aria-label="Y a un jardin"]').clear().type(garden);
 
         cy.get('#result').invoke('text').then(parseInt).should('eq', price);
     });
@@ -21,9 +21,9 @@ describe('Case 1', () => {
         houses.slice(1).forEach(([size, nb_rooms, garden, _, price]) => {
             cy.visit('/');
 
-            cy.get('input[aria-label="Taille maison"]').type(size);
-            cy.get('input[aria-label="Nombre de chambre"]').type(nb_rooms);
-            cy.get('input[aria-label="Y a un jardin"]').type(garden);
+            cy.get('input[aria-label="Taille maison"]').clear().type(size);
+            cy.get('input[aria-label="Nombre de chambre"]').clear().type(nb_rooms);
+            cy.get('input[aria-label="Y a un jardin"]').clear().type(garden);
 
             cy.get('#result').invoke('text').then(parseInt).should('eq', price);
         });

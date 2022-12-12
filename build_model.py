6b396c10d6956cc55bcc85e5c434842413e54921
Aurélien GARNIER <au2001sfg@gmail.com>
2022-12-12 13:26:27 +0100

    Fix prediction

--- build_model.py
+++ build_model.py
@@ -1,15 +1,17 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
 
 def build_model():
-    import pandas as pd 
-    from sklearn.linear_model import LinearRegression
-    import joblib
     df = pd.read_csv('houses.csv')
+
     X = df[['size', 'nb_rooms', 'garden']]
     y = df['price']
+
     model = LinearRegression()
     model.fit(X, y)
-    joblib.dump(model, "regression.joblib")
-    
-        
-    
-build_model()
+
+    joblib.dump(model, 'regression.joblib')
+
+if __name__ == '__main__':
+    build_model()
